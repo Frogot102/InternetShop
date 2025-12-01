1e819708e4df50029d23bfc475c3819eb1a3b1a1
Frogot1 <alfiagilmutdinova30@gmail.com>
2025-12-01 09:24:56 +0300

    3123

--- InternetShop/Program.cs
+++ InternetShop/Program.cs
@@ -7,40 +7,13 @@ using System.Text;
 var builder = WebApplication.CreateBuilder(args);
 
 
-builder.Services.AddControllers()
-    .AddJsonOptions(options =>
-    {
-        options.JsonSerializerOptions.ReferenceHandler = System.Text.Json.Serialization.ReferenceHandler.IgnoreCycles;
-    });
-
+builder.Services.AddControllers();
 
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen(c =>
 {
-    c.AddSecurityDefinition("Bearer", new OpenApiSecurityScheme
-    {
-        Name = "Authorization",
-        Type = SecuritySchemeType.Http,
-        Scheme = "bearer",
-        BearerFormat = "JWT",
-        In = ParameterLocation.Header,
-        Description = "JWT Authorization header using the Bearer scheme."
-    });
 
-    c.AddSecurityRequirement(new OpenApiSecurityRequirement
-    {
-        {
-            new OpenApiSecurityScheme
-            {
-                Reference = new OpenApiReference
-                {
-                    Type = ReferenceType.SecurityScheme,
-                    Id = "Bearer"
-                }
-            },
-            Array.Empty<string>()
-        }
-    });
+    c.SwaggerDoc("v1", new OpenApiInfo { Title = "InternetShop API", Version = "v1" });
 });
 
 
@@ -66,18 +39,18 @@ builder.Services.AddAuthentication("Bearer")
 
 var app = builder.Build();
 
-
 if (app.Environment.IsDevelopment())
 {
     app.UseSwagger();
-    app.UseSwaggerUI();
+    app.UseSwaggerUI(c =>
+    {
+        c.SwaggerEndpoint("/swagger/v1/swagger.json", "InternetShop API v1");
+    });
 }
 
 app.UseHttpsRedirection();
-
-app.UseAuthentication(); 
-app.UseAuthorization();  
-
+app.UseAuthentication();  
+app.UseAuthorization();   
 app.MapControllers();
 
 app.Run();
